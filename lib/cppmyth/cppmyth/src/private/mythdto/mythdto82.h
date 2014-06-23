8d2a5262a44eee2803a2fd3c3137466a7645235f
janbar <jlbarriere68@gmail.com>
2014-06-23 22:59:45 +0200

    [cppmyth]  update FSF address
    The Free Software Foundation's mailing address has changed. Update the
    mailing address in each header.

--- lib/cppmyth/cppmyth/src/private/mythdto/mythdto82.h
+++ lib/cppmyth/cppmyth/src/private/mythdto/mythdto82.h
@@ -1,8 +1,22 @@
-/* 
- * File:   mythdto82.h
- * Author: jlb
+/*
+ *      Copyright (C) 2014 Jean-Luc Barriere
  *
- * Created on 21 juin 2014, 01:44
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+ *  MA 02110-1301 USA
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
  */
 
 #ifndef MYTHDTO82_H
